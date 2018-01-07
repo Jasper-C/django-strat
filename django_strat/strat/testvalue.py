d2f676e40c4e8f46f85caef66425f22d211a7400
Daniel Avila <sparkytwobillion@gmail.com>
2018-01-07 14:23:11 -0800

    Adding test faluse here

---
+++ django_strat/strat/testvalue.py
@@ -0,0 +1,54 @@
+teams = """
+    <table border="1">
+      <tr>
+        <th colspan="2">Team</th>
+        <th>Owner</th>
+        <th>Ballpark</th>
+        <th>Division</th>
+      </tr>
+      <tr>
+        <td>
+          <img src="/static/league/2017T0000.jpg"
+          alt="Default Team Location 000 Default Team Name 000 Logo" />
+        </td>
+        <td>
+          <a href="/league/teams/2017/T0000/">Default Team Location
+          000 Default Team Name 000</a>
+        </td>
+        <td>
+          <a href="mailto:"></a>
+        </td>
+        <td>2017 Default Ballpark Location 000</td>
+        <td>AL West</td>
+      </tr>
+      <tr>
+        <td>
+          <img src="/static/league/2017T0001.jpg"
+          alt="Default Team Location 001 Default Team Name 001 Logo" />
+        </td>
+        <td>
+          <a href="/league/teams/2017/T0001/">Default Team Location
+          001 Default Team Name 001</a>
+        </td>
+        <td>
+          <a href="mailto:"></a>
+        </td>
+        <td>2017 Default Ballpark Location 001</td>
+        <td>AL West</td>
+      </tr>
+      <tr>
+        <td>
+          <img src="/static/league/2017T0002.jpg"
+          alt="Default Team Location 002 Default Team Name 002 Logo" />
+        </td>
+        <td>
+          <a href="/league/teams/2017/T0002/">Default Team Location
+          002 Default Team Name 002</a>
+        </td>
+        <td>
+          <a href="mailto:"></a>
+        </td>
+        <td>2017 Default Ballpark Location 002</td>
+        <td>AL West</td>
+      </tr>
+    </table>"""
