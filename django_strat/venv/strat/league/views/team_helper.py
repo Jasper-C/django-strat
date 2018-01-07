2f82f55a71386f8f89c292d97b9cda32ed784363
Jasper-C <jasper.cattin@gmail.com>
2018-01-07 10:35:40 -0800

    update

--- django_strat/venv/strat/league/views/team_helper.py
+++ django_strat/venv/strat/league/views/team_helper.py
@@ -1,7 +1,31 @@
-django.shortupts import get_list_or_494
+from django.db.models import Q
 
-from league.models.teams import Roster
+from league.models.teams import Payroll
 
-def collect_roster(franchise):
-    roster = get_list_or_404(Roster, franchise=franchise)
-    return roster
+
+def collect_payroll_adjustments(franchise, year):
+    adjustments = Payroll.objects.filter(
+        Q(paying=franchise) | Q(receiving=franchise)
+    )
+    adjustments = adjustments.filter(year__gte=year).order_by('note', 'year')
+    return_data = []
+    for a in adjustments:
+        notes = a.note
+        money = a.money
+        if a.receiving is not None:
+            if a.receiving.id == franchise:
+                money = a.money * -1
+        exists = False
+        for r in return_data:
+            if r['notes'] == notes:
+                exists = True
+        if exists:
+            for r in return_data:
+                if r['notes'] == notes:
+                    r['adjustments'].append(money)
+        else:
+            return_data.append({
+                'notes': notes,
+                'adjustments': [money, ],
+            })
+    return return_data
--- django_strat/venv/strat/league/views/teams.py
+++ django_strat/venv/strat/league/views/teams.py
@@ -1,4 +1,4 @@
-from django.views import generic
+from django.views import View
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.db.models import Q
@@ -6,19 +6,25 @@ from django.db.models import Q
 from league.models.teams import Team, Payroll
 from league.models.players import Player, Contract
 
+from league.views.team_helper import collect_payroll_adjustments
+
 
 class TeamIndex(View):
 
-    def get(self, request, year):
-        if year is None:
-            year = timezone.now().year
+    def get(self, request, year=timezone.now().year):
+        # if year is None:
+        #     year = timezone.now().year
         team_list = Team.objects.filter(year=year).order_by('division', 'location')
         year_list = set(Team.objects.all().values_list("year", flat=True))
+        next_year = year + 1
+        last_year = year - 1
 
         context = {
             'team_list': team_list,
             'year_list': year_list,
             'year': year,
+            'last_year': last_year,
+            'next_year': next_year,
         }
         return render(request, 'league/team/index.html', context)
 
@@ -50,14 +56,12 @@ def team_detail(request, year, abbreviation):
 def team_contracts(request, year, abbreviation):
     team = get_object_or_404(Team, year=year, abbreviation=abbreviation)
     contracts = Contract.objects.filter(year=year, team=abbreviation)
-    payroll = Payroll.objects.filter(
-        Q(paying=abbreviation) | Q(receiving=abbreviation)
-    )
-    payroll = payroll.filter(year__gte=year).order_by('year', 'note')
+    payroll = collect_payroll_adjustments(abbreviation, year)
     context = {
         'team': team,
         'contracts': contracts,
         'payroll': payroll,
-        'contract_list': ['Y1', 'Y2', 'Y3', 'Arb4', 'Arb5', 'Arb6', 'FA']
+        'contract_list': ['Y1', 'Y2', 'Y3', 'Arb4', 'Arb5', 'Arb6', 'FA'],
+        'salary_cap': [135000000, 135000000, 135000000, 135000000, 135000000, 135000000]
     }
     return render(request, 'league/team/contracts.html', context)
